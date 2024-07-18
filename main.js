8635e9689613b00a3f2fdce28f8967a13f8a5399
isma <ismaelmartin302@gmail.com>
2024-07-18 20:15:03 +0200

    funciona pero pone siempre undefined al principio

---
+++ main.js
@@ -0,0 +1,4 @@
+import { TransformScripts } from "./scripts/transformScripts.js";
+
+
+console.log(TransformScripts.encriptBasic(prompt("Dime una frase")))
---
+++ scripts/transformScripts.js
@@ -0,0 +1,24 @@
+class TransformScripts {
+
+    static encriptBasic(text) {
+        let alphabet = "abcdefghijklmnopqrstuvwxyz"
+        let alphabetArray = []
+        for (let index = 0; index < alphabet.length; index++) {
+            alphabetArray.push(alphabet[index]);
+        }
+
+        let alterText
+        for (let index = 0; index < text.length; index++) {
+            let value = alphabetArray.indexOf(text[index]);
+            if (value == alphabetArray.length) {
+                value = 0
+            }
+            alterText += alphabetArray[value + 1]
+        }
+
+        return alterText
+    }
+}
+
+
+export { TransformScripts }

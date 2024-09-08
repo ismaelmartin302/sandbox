73cab638666a0e2ecb923ffd67c326ffba97ee0a
isma <ismaelmartin302@gmail.com>
2024-09-08 22:20:39 +0200

    "Moved HTML elements from index.html to main.js and replaced with new H1 element in index.html"

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 import { Cursor } from "./scripts/cursor.js";
 import { TransformScripts } from "./scripts/transformScripts.js";
+const body = document.querySelector('body')
+body.innerHTML = `
+    <div id="test1">
+        <button onclick="TransformScripts.encriptBasic(prompt('Dime una frase'))">Invertir order</button>
+        </div>
+        <div class="scrollBarPercentageIndicator">
+    </div>
+`;

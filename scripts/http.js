b828dad5f661061e4e8df8c68c6f1b763073eb5b
isma <ismaelmartin302@gmail.com>
2024-09-28 00:40:00 +0200

    "Extracted request handling logic into separate function"

--- scripts/http.js
+++ scripts/http.js
@@ -2,14 +2,16 @@ const http = require('node:http')
 
 const desiredPort = process.env.PORT ?? 1234
 
-const server = http.createServer((req, res) => {
+const processRequest = (req, res) => {
     console.log('request receveid:', req.url)
     if (req.url === '/') {
         res.end('Holaaa!!! pon algo en la URL')
     } else {
         res.end(`Has puesto: '${req.url}' en la URL \n ${req}\n ${res.statusCode}`)
     }
-})
+}
+
+const server = http.createServer(processRequest)
 server.listen(desiredPort, () => {
     console.log(`Servidor escuchando en el puerto http://localhost:${desiredPort}`)
 })

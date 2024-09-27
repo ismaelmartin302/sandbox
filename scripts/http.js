654b21ea27a663d9b7732361af611d97059e0c60
isma <ismaelmartin302@gmail.com>
2024-09-28 00:45:29 +0200

    "Moved Content-Type header setting to top of processRequest function and added 404 response for non-root URLs"

--- scripts/http.js
+++ scripts/http.js
@@ -4,10 +4,13 @@ const desiredPort = process.env.PORT ?? 1234
 
 const processRequest = (req, res) => {
     console.log('request receveid:', req.url)
+    res.setHeader('Content-Type', 'text/plain; charset=utf8')
     if (req.url === '/') {
         res.statusCode = 200
-        res.setHeader('Content-Type', 'text/plain; charset=utf8')
         res.end('Homeàaa')
+    } else {
+        res.statusCode = 404
+        res.end('Not found')
     }
 }
 

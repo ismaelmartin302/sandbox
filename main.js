de6a5aa66ed840e3dad9b0fa98fccae6e9d08a3e
isma <ismaelmartin302@gmail.com>
2024-10-02 22:09:03 +0200

    "Removed imports and HTML loading code from main.js, added EjerciciosAlgoritmos class to algoritmia.js, and updated package.json with new script and type."

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-import { Cursor } from './scripts/cursor.js'
-import { TransformScripts } from './scripts/transformScripts.js'
-const body = document.querySelector('body')
-fetch('pages/app.html')
-    .then(response => response.text()) // Convertir la respuesta a texto
-    .then(html => {
-        body.innerHTML = html // Insertar el contenido HTML en el body
-    })
-    .catch(error => {
-        console.error('Error al cargar el archivo HTML:', error)
-    })
+/* import { Cursor } from './scripts/cursor.js'
+import { TransformScripts } from './scripts/transformScripts.js' */
+
+import { EjerciciosAlgoritmos } from './scripts/algoritmia.js'
+
+console.log(EjerciciosAlgoritmos.ejercicio1())

32c92d3a00e553bbd7baf043589dbde97a8832bb
isma <ismaelmartin302@gmail.com>
2024-10-02 21:08:03 +0200

    "Updated package-lock.json and package.json, modified scripts/cursor.js and scripts/transformScripts.js, and deleted scripts/physics.js"

--- scripts/transformScripts.js
+++ scripts/transformScripts.js
@@ -1,16 +1,15 @@
 class TransformScripts {
-
     static encriptBasic(text) {
-        let alphabet = "abcdefghijklmnopqrstuvwxyz"
-        let alphabetArray = []
+        const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        const alphabetArray = []
         for (let index = 0; index < alphabet.length; index++) {
-            alphabetArray.push(alphabet[index]);
+            alphabetArray.push(alphabet[index])
         }
 
-        let alterText = ""
+        let alterText = ''
         for (let index = 0; index < text.length; index++) {
-            let value = alphabetArray.indexOf(text[index]);
-            if (value == alphabetArray.length) {
+            let value = alphabetArray.indexOf(text[index])
+            if (value === alphabetArray.length) {
                 value = 0
             }
             alterText += alphabetArray[value + Math.round(Math.random() * 10)]
@@ -19,6 +18,5 @@ class TransformScripts {
         return alterText
     }
 }
-
 
 export { TransformScripts }
